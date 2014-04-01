cb4ddcaa3fb326d9e7b0db6923bb47a59db66bcb
Pepa P <emp3rr@gmail.com>
2014-04-01 18:02:48 +0200

    .

--- ZP4JV/Class04/Class4.java
+++ ZP4JV/Class04/Class4.java
@@ -1,11 +1,9 @@
 package Class04;
 
-import javax.swing.JFrame;
-
 public class Class4 {
 
 	public static void main(String[] args) {
-		JFrame form = new Form();
+		Form form = new Form();
 		form.setVisible(true);
 	}
 }
